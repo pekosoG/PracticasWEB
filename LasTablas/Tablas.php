cdf26990fce15e6972bf1aac7093fc01a581a0af
pekoso Garcia <israel_gahe@hotmail.com>
2014-02-13 09:01:13 -0600

    Modificacion de Codigo, siguiendo las especificaciones de la maestra para mejorar performance

--- LasTablas/Tablas.php
+++ LasTablas/Tablas.php
@@ -1,9 +1,8 @@
 <?php
 	for($i=1;$i<=12;$i++){
-		echo "Tabla del $i".'</br>----------------</br>';
+		echo "Tabla del $i",'</br>----------------</br>';
 		for($j=1;$j<=10;$j++){
-			$k=$i*$j;
-			echo "{$i}x{$j}={$k}".'</br>';
+			echo "{$i}x{$j}=",$i*$j,'</br>';
 		}
 		echo '</br>';
 	}
